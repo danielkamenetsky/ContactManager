c0e9fc8332821830890c417b84355e334f72b039
Daniel Kamenetsky <20dk19@queensu.ca>
2024-09-25 15:10:12 -0400

    Updated commit

--- contact_manager/contacts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.contact_list, name='contact_list'),  # URL for the contact list
-    # You can add more URL patterns here for other views related to contacts
-]
