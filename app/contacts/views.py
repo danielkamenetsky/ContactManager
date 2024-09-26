b46b385d4d4c3760b88739e82a478341a4985a15
Daniel Kamenetsky <20dk19@queensu.ca>
2024-09-26 10:34:11 -0400

    Updated templates

--- app/contacts/views.py
+++ app/contacts/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 # Contains views (handlers for HTTP requests) which handle what happens when a user accesses different URLs (i.e. rendering a template, queryingthe database)
 def home(request):
-    return HttpResponse('<h1>Hello<h1>')
+    return render(request, 'home.html')
 
 def contact_list(request):
-    return HttpResponse('<h1>Bye<h1>')
+    return render(request, 'contact_list.html')
